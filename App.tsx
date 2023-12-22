62aa53ba4f3f70c7b9d93f8ac2e708b30c7556fe
Semion Krapivin <homer1996@gmail.com>
2023-12-22 11:27:24 +0100

    style: add some flexboxes

--- App.tsx
+++ App.tsx
@@ -1,37 +1,32 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          margin: 10,
-          borderWidth: 2,
-          borderColor: "blue",
-          padding: 16,
-          borderRadius: 10,
-        }}
-      >
-        Hello
-      </Text>
-      <Text style={styles.text}>Hello</Text>
-      <Button title="Click me" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your goal" style={styles.textInput} />
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 40,
   },
-  text: {
-    margin: 10,
-    borderWidth: 2,
-    borderColor: "green",
-    padding: 16,
-    borderRadius: 5,
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    marginRight: 8,
+    padding: 8,
   },
 });
