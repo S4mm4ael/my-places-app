e6cd860b504c382485156d331748a46adf2f03c6
Semion Krapivin <homer1996@gmail.com>
2023-12-22 11:06:45 +0100

    style: move inline styling example to obkect

--- App.tsx
+++ App.tsx
@@ -4,10 +4,17 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text
-        style={{ margin: 10, borderWidth: 2, borderColor: "blue", padding: 16 }}
+        style={{
+          margin: 10,
+          borderWidth: 2,
+          borderColor: "blue",
+          padding: 16,
+          borderRadius: 10,
+        }}
       >
         Hello
       </Text>
+      <Text style={styles.text}>Hello</Text>
       <Button title="Click me" />
     </View>
   );
@@ -19,5 +26,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    margin: 10,
+    borderWidth: 2,
+    borderColor: "green",
+    padding: 16,
+    borderRadius: 5,
   },
 });
