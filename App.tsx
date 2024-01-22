efd9dbfffccc7ae0c646c330752dc3ca68723d94
Semion Krapivin <homer1996@gmail.com>
2024-01-22 12:31:33 +0100

    init: new project setup

--- App.tsx
+++ App.tsx
@@ -2,66 +2,14 @@ import { StyleSheet, View, SafeAreaView, FlatList, Button } from "react-native";
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 
-import GoalItem, { IItemData } from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [goals, setGoals] = useState<Array<{ text: string; id: string }>>([]);
-
-  function startAddGoalHandling() {
-    setShowModal(true);
-  }
-
-  function endAddGoalHandling() {
-    setShowModal(false);
-  }
-
-  function inputButtonHandler(enteredText: string) {
-    setGoals((currentGoals) => [
-      ...currentGoals,
-      { text: enteredText, id: Math.random().toString() },
-    ]);
-    endAddGoalHandling();
-  }
-
-  function handleGoalDelete(id: string) {
-    setGoals((currentGoals) => {
-      return currentGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
-  const renderGoalItem = (data: IItemData) => (
-    <GoalItem data={data} deleteItem={handleGoalDelete} />
-  );
-
   return (
     <>
       <StatusBar style="dark" />
       <SafeAreaView>
-        <View style={styles.appContainer}>
-          <Button
-            title="Add new goal"
-            color={"#5e0acc"}
-            onPress={startAddGoalHandling}
-          />
-          <GoalInput
-            isVisible={showModal}
-            onAdd={inputButtonHandler}
-            onCancel={endAddGoalHandling}
-          />
-          <View>
-            <FlatList<IItemData>
-              data={goals}
-              style={styles.goalsContainer}
-              alwaysBounceVertical={false}
-              renderItem={(goalItem) => renderGoalItem(goalItem.item)}
-              keyExtractor={(item) => {
-                return item.id;
-              }}
-            ></FlatList>
-          </View>
-        </View>
+        <View style={styles.appContainer}></View>
       </SafeAreaView>
     </>
   );
@@ -73,5 +21,4 @@ const styles = StyleSheet.create({
     paddingVertical: 50,
     paddingHorizontal: 10,
   },
-  goalsContainer: {},
 });
