9b1f33633eecb0f89f4c014278cc462486597fd3
Semion Krapivin <homer1996@gmail.com>
2024-01-25 10:41:22 +0100

    feat: add linear gradient component to app

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet } from "react-native";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={["#ff84c1", "#ddb52f"]} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ec9cc4",
   },
 });
