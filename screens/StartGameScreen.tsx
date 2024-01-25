059b2b643f43f77622af70f4930976eb8b348c95
Semion Krapivin <homer1996@gmail.com>
2024-01-25 09:36:26 +0100

    feat: add number pad styling

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton buttonText="Reset" />
       <PrimaryButton buttonText="Confirm" />
     </View>
@@ -27,5 +33,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 56,
+    width: 56,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
