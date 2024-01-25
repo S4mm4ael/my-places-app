17bb012ede7931a2e903e77d586f1986758947c6
Semion Krapivin <homer1996@gmail.com>
2024-01-25 10:09:28 +0100

    styles: added to primaryButton

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 interface IPrimaryButtonProps {
   buttonText: string;
@@ -6,10 +6,47 @@ interface IPrimaryButtonProps {
 
 function PrimaryButton({ buttonText }: IPrimaryButtonProps) {
   return (
-    <View>
-      <Text>{buttonText}</Text>
+    <View style={styles.primaryButtonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.primaryButtonInnerContainer]
+            : styles.primaryButtonInnerContainer
+        }
+        android_ripple={{ color: "#2b141f" }}
+      >
+        <Text style={styles.primaryButtonText}>{buttonText}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  primaryButtonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  primaryButtonInnerContainer: {
+    backgroundColor: "#7e3b5c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    //Android specific:
+    elevation: 8,
+    //IOs
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  primaryButtonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  //IOs
+  pressed: {
+    opacity: 0.75,
+  },
+});
