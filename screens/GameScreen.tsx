38a4bc6100b7ade3e9cd4d2813e6f3c2c454d085
Semion Krapivin <homer1996@gmail.com>
2024-01-29 16:09:56 +0100

    feat: gamescreen content

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,5 +1,22 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function GameScreen() {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent guess</Text>
+      <Text>GUESS</Text>
+
+      <View>
+        <Text>Higher or lower</Text>
+        <Text>+-</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 16,
+  },
+});
