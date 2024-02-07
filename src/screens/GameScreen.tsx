2e3d72c3dc509e8eb12a34aa21100ffc5a86ee92
Semion Krapivin <homer1996@gmail.com>
2024-02-07 20:08:48 +0100

    feat: icons added

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,4 +1,6 @@
-import { Text, View, StyleSheet, Alert } from "react-native";
+import { Text, View, StyleSheet, Alert, Pressable } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import { generateRandomNumberBetween } from "../utils/helpers/generateRandomNumber";
 import { useState, useEffect, useMemo } from "react";
 import { Title } from "../components/UI/Title";
@@ -7,6 +9,7 @@ import PrimaryButton from "../components/UI/PrimaryButton";
 import Card from "../components/UI/Card";
 import HorizontalButtonsContainer from "../components/UI/HorizontalButtonsContainer";
 import InstructionText from "../components/UI/InstructionText";
+import { colors } from "../global/constatnts";
 
 interface GameScreenProps {
   userNumber: number;
@@ -16,14 +19,8 @@ interface GameScreenProps {
 let minBoundary = 1;
 let maxBoundary = 100;
 
-export default function GameScreen({
-  userNumber,
-  onGameOver,
-}: GameScreenProps) {
-  const initialGuess = useMemo(
-    () => generateRandomNumberBetween(1, 100, userNumber),
-    [userNumber]
-  );
+export default function GameScreen({ userNumber, onGameOver }: GameScreenProps) {
+  const initialGuess = useMemo(() => generateRandomNumberBetween(1, 100, userNumber), [userNumber]);
 
   const [currentGuess, setCurrenGuess] = useState(initialGuess);
 
@@ -38,9 +35,7 @@ export default function GameScreen({
       (direction === "lower" && currentGuess < userNumber) ||
       (direction === "higher" && currentGuess > userNumber)
     ) {
-      Alert.alert("Dont lie!", "You know that it is wrong!", [
-        { text: "Sorry!", style: "cancel" },
-      ]);
+      Alert.alert("Dont lie!", "You know that it is wrong!", [{ text: "Sorry!", style: "cancel" }]);
       return;
     }
     if (direction === "lower") {
@@ -48,11 +43,7 @@ export default function GameScreen({
     } else {
       minBoundary = currentGuess + 1;
     }
-    const newRndNumber = generateRandomNumberBetween(
-      minBoundary,
-      maxBoundary,
-      currentGuess
-    );
+    const newRndNumber = generateRandomNumberBetween(minBoundary, maxBoundary, currentGuess);
     setCurrenGuess(newRndNumber);
   }
 
@@ -61,22 +52,17 @@ export default function GameScreen({
       <Title text="Opponent Guess" />
       <NumberContainer number={currentGuess} />
       <Card>
-        <InstructionText
-          text="Higher or lover"
-          style={styles.instructionText}
-        />
+        <InstructionText text="Higher or lover" style={styles.instructionText} />
         <HorizontalButtonsContainer>
           <View style={styles.buttonContainer}>
-            <PrimaryButton
-              buttonText="-"
-              onPress={() => nextGuessHandler("lower")}
-            />
+            <Pressable onPress={() => nextGuessHandler("lower")}>
+              <Ionicons name="md-remove" size={24} color={colors.White} />
+            </Pressable>
           </View>
           <View style={styles.buttonContainer}>
-            <PrimaryButton
-              buttonText="+"
-              onPress={() => nextGuessHandler("higher")}
-            />
+            <Pressable onPress={() => nextGuessHandler("higher")}>
+              <Ionicons name="md-add" size={24} color={colors.White} />
+            </Pressable>
           </View>
         </HorizontalButtonsContainer>
       </Card>
