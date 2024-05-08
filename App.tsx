0cca0deb9ee606cf2f45a53309820e620ce53f45
Krapivin Siamion <homer1996@gmail.com>
2024-05-08 13:37:17 +0200

    init: old project cleanup

--- App.tsx
+++ App.tsx
@@ -1,86 +1,19 @@
-import { useCallback, useState } from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import { ImageBackground, SafeAreaView, StyleSheet } from "react-native";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
-
-import { colors } from "./src/global/constatnts";
-import GameScreen from "./src/screens/GameScreen";
-import StartGameScreen from "./src/screens/StartGameScreen";
-import GameOverScreen from "./src/screens/GameOverScreen";
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
 
 export default function App() {
-  const [fontsLoaded, fontError] = useFonts({
-    "Inter-Bold": require("./src/assets/fonts/Inter-Bold.ttf"),
-  });
-
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  const [userNumber, setUserNumber] = useState<number | null>();
-  const [gameIsOver, setGameIsOver] = useState<boolean>();
-  const [guessRounds, setGuessRounds] = useState<number>();
-
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
-
-  function pickedNumberHandler(pickedNumber: number) {
-    setUserNumber(pickedNumber);
-    setGameIsOver(false);
-  }
-
-  function gameOverHandler(numberOfRounds: number) {
-    setGameIsOver(true);
-    setGuessRounds(numberOfRounds);
-  }
-
-  function startNewGameHandler() {
-    setUserNumber(null);
-    setGameIsOver(false);
-    setGuessRounds(0);
-  }
-
-  if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
-  }
-
-  if (gameIsOver && userNumber) {
-    screen = (
-      <GameOverScreen
-        roundsNumber={guessRounds ?? 0}
-        userNumber={userNumber}
-        onStartNewGame={startNewGameHandler}
-      />
-    );
-  }
-
   return (
-    <>
-      <StatusBar style="auto" />
-      <LinearGradient
-        colors={[colors.GradientPink, colors.GradientYellow]}
-        style={styles.rootScreen}
-        onLayout={onLayoutRootView}
-      >
-        <ImageBackground
-          source={require("./src/assets/images/appBackground.jpg")}
-          resizeMode="cover"
-          style={styles.rootScreen}
-          imageStyle={styles.backgroundImage}
-        >
-          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-        </ImageBackground>
-      </LinearGradient>
-    </>
+    <View style={styles.rootScreen}>
+      <Text>Hello</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   backgroundImage: {
