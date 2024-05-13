48dc7bca2ed054e6a6d0f5642a565fa36dd7d128
Siamion Krapivin <homer1996@gmail.com>
2024-05-13 11:35:02 +0200

    feat: add image styling

--- components/MealItem.tsx
+++ components/MealItem.tsx
@@ -1,6 +1,7 @@
-import {View, Text} from "react-native";
+import {View, Text, Pressable, Image, StyleSheet, Platform} from "react-native";
 import React from "react";
 import Meal from "../models/meal";
+import {stylesGlobal} from "../global/styles";
 
 interface IProps {
   data: Meal;
@@ -8,10 +9,67 @@ interface IProps {
 
 const MealItem = ({data}: IProps) => {
   return (
-    <View>
-      <Text>{data.title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        style={({pressed}) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{color: "ccc"}}
+        onPress={() => null}
+      >
+        <View>
+          <Image source={{uri: data.imageUrl}} style={styles.image} />
+          <Text style={styles.title}>{data.title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailsItem}>{data.duration} min</Text>
+          <Text style={styles.detailsItem}>
+            {data.complexity.toUpperCase()}
+          </Text>
+          <Text style={styles.detailsItem}>
+            {data.affordability.toUpperCase()}
+          </Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 12,
+    backgroundColor: "white",
+    borderRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    ...stylesGlobal.itemShadow,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 4,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailsItem: {
+    margin: 4,
+    fontSize: 12,
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
---
+++ global/styles.ts
@@ -0,0 +1,9 @@
+export const stylesGlobal = {
+  itemShadow: {
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 8,
+  },
+};
