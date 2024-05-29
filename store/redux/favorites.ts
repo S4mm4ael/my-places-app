449c250306847310e0eda11281cb9c7d9476bd80
Siamion Krapivin <homer1996@gmail.com>
2024-05-29 23:02:22 +0200

    refactor: typization fixed

--- store/redux/favorites.ts
+++ store/redux/favorites.ts
@@ -1,15 +1,19 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {PayloadAction, createSlice} from "@reduxjs/toolkit";
+
+interface IFavoritesPayload {
+  id: string;
+}
 
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState: {
-    ids: [],
+    ids: [] as string[],
   },
   reducers: {
-    addFavorite: (state, action) => {
+    addFavorite: (state, action: PayloadAction<IFavoritesPayload>) => {
       state.ids.push(action.payload.id);
     },
-    removeFavorite: (state, action) => {
+    removeFavorite: (state, action: PayloadAction<IFavoritesPayload>) => {
       state.ids.splice(state.ids.indexOf(action.payload.id), 1);
     },
   },
