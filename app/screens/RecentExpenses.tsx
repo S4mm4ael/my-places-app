24c5601a464895f044cf74077b2bf2d9489a7fb0
Siamion Krapivin <homer1996@gmail.com>
2024-07-01 13:17:06 +0300

    refactor: Use ExpensesContext in AllExpenses and RecentExpenses screens

--- app/screens/RecentExpenses.tsx
+++ app/screens/RecentExpenses.tsx
@@ -1,12 +1,27 @@
 import {View, Text} from "react-native";
-import React from "react";
+import React, {useContext} from "react";
 import {ExpensesOutput} from "../components/Expenses";
 import {mockedExpenses} from "../components/Expenses/data";
+import {ExpensesContext} from "../stores/expenses-context";
 
 const expensesName = "Recent 7 days";
 
 export const RecentExpenses = () => {
+  const {expenses} = useContext(ExpensesContext);
+
+  const filteredExpenses = expenses.filter((expense) => {
+    const expenseDate = new Date(expense.date);
+    const currentDate = new Date();
+
+    const secondsInDay = 1000 * 60 * 60 * 24;
+
+    const diffTime = Math.abs(currentDate.getTime() - expenseDate.getTime());
+    const diffDays = Math.ceil(diffTime / secondsInDay);
+
+    return diffDays <= 7;
+  });
+
   return (
-    <ExpensesOutput expenses={mockedExpenses} expensesName={expensesName} />
+    <ExpensesOutput expenses={filteredExpenses} expensesName={expensesName} />
   );
 };
