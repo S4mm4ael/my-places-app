9ca4741e729d1e1212b25fd018dfdb3f98016b6d
Siamion Krapivin <homer1996@gmail.com>
2024-07-16 16:31:52 +0200

    refactor: RecentExpenses to use lazy loading for fetching and displaying expenses

--- app/screens/RecentExpenses.tsx
+++ app/screens/RecentExpenses.tsx
@@ -4,6 +4,7 @@ import {ExpensesContext} from "../stores/expenses-context";
 import {getRecentExpenses} from "./expensesUtils";
 import {fetchExpenses} from "../utils/api";
 import {Expense} from "../constants";
+import {LoadingOverlay} from "../components/UI/LoadingOverlay";
 
 const expensesName = "Recent 7 days";
 
@@ -11,14 +12,22 @@ export const RecentExpenses = () => {
   const {expenses, setExpenses: setExpensesLocally} =
     useContext(ExpensesContext);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
     async function getExpenses() {
+      setIsLoading(true);
       const expenses = await fetchExpenses();
       setExpensesLocally(expenses);
+      setIsLoading(false);
     }
 
     getExpenses();
   }, []);
+
+  if (isLoading) {
+    return <LoadingOverlay />;
+  }
 
   return (
     <ExpensesOutput
