a021eeac93540e94d7c346940de863a76df02bb0
Siamion Krapivin <homer1996@gmail.com>
2024-07-18 17:09:07 +0200

    init: clean project

--- app/components/Expenses/index.ts
+++
@@ -1,4 +0,0 @@
-export {ExpensesList} from "./ExpensesList";
-export {ExpensesOutput} from "./ExpensesOutput";
-export {ExpensesSummary} from "./ExpensesSummary";
-export {ExpenseItem} from "./ExpenseItem";
--- app/constants/models.ts
+++
@@ -1,6 +0,0 @@
-export interface Expense {
-  id: string;
-  description: string;
-  amount: number;
-  date: Date;
-}
