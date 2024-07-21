a99be2fc5408d39d4d60fe6843354827260be0a1
Siamion Krapivin <homer1996@gmail.com>
2024-07-22 00:23:14 +0200

    refactor: Optimize AuthContent component and improve navigation

--- components/Auth/AuthContent.js
+++ components/Auth/AuthContent.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import {useState} from "react";
+import {Alert, StyleSheet, View} from "react-native";
 
-import FlatButton from '../ui/FlatButton';
-import AuthForm from './AuthForm';
-import { Colors } from '../../constants/styles';
+import FlatButton from "../ui/FlatButton";
+import AuthForm from "./AuthForm";
+import {Colors} from "../../constants/styles";
+import {useNavigation} from "@react-navigation/native";
 
-function AuthContent({ isLogin, onAuthenticate }) {
+function AuthContent({isLogin, onAuthenticate}) {
+  const {replace} = useNavigation();
 
   const [credentialsInvalid, setCredentialsInvalid] = useState({
     email: false,
@@ -15,16 +17,20 @@ function AuthContent({ isLogin, onAuthenticate }) {
   });
 
   function switchAuthModeHandler() {
-    // Todo
+    if (isLogin) {
+      replace("Signup");
+    } else {
+      replace("Login");
+    }
   }
 
   function submitHandler(credentials) {
-    let { email, confirmEmail, password, confirmPassword } = credentials;
+    let {email, confirmEmail, password, confirmPassword} = credentials;
 
     email = email.trim();
     password = password.trim();
 
-    const emailIsValid = email.includes('@');
+    const emailIsValid = email.includes("@");
     const passwordIsValid = password.length > 6;
     const emailsAreEqual = email === confirmEmail;
     const passwordsAreEqual = password === confirmPassword;
@@ -34,7 +40,7 @@ function AuthContent({ isLogin, onAuthenticate }) {
       !passwordIsValid ||
       (!isLogin && (!emailsAreEqual || !passwordsAreEqual))
     ) {
-      Alert.alert('Invalid input', 'Please check your entered credentials.');
+      Alert.alert("Invalid input", "Please check your entered credentials.");
       setCredentialsInvalid({
         email: !emailIsValid,
         confirmEmail: !emailIsValid || !emailsAreEqual,
@@ -43,7 +49,7 @@ function AuthContent({ isLogin, onAuthenticate }) {
       });
       return;
     }
-    onAuthenticate({ email, password });
+    onAuthenticate({email, password});
   }
 
   return (
@@ -55,7 +61,7 @@ function AuthContent({ isLogin, onAuthenticate }) {
       />
       <View style={styles.buttons}>
         <FlatButton onPress={switchAuthModeHandler}>
-          {isLogin ? 'Create a new user' : 'Log in instead'}
+          {isLogin ? "Create a new user" : "Log in instead"}
         </FlatButton>
       </View>
     </View>
@@ -72,8 +78,8 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     backgroundColor: Colors.primary800,
     elevation: 2,
-    shadowColor: 'black',
-    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "black",
+    shadowOffset: {width: 1, height: 1},
     shadowOpacity: 0.35,
     shadowRadius: 4,
   },
