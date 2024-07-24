59a1cd8b5188998d37255283a28913ff54badbc5
Siamion Krapivin <homer1996@gmail.com>
2024-07-24 14:33:06 +0200

    refactor: Add logout button to Welcome screen header

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import WelcomeScreen from "./screens/WelcomeScreen";
 import {Colors} from "./constants/styles";
 import AuthContextProvider, {AuthContext} from "./store/auth-context";
 import {useContext} from "react";
+import IconButton from "./components/ui/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -31,6 +32,8 @@ function AuthStack() {
 }
 
 function AuthenticatedStack() {
+  const {logout} = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -39,7 +42,20 @@ function AuthenticatedStack() {
         contentStyle: {backgroundColor: Colors.primary100},
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({tintColor}) => (
+            <IconButton
+              icon="exit"
+              color={tintColor}
+              onPress={logout}
+              size={20}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
