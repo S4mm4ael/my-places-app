6014f05efc40ef36c95c347c8960940636ebc10d
Siamion Krapivin <homer1996@gmail.com>
2024-08-02 16:22:39 +0200

    refactor: Update PlaceForm component to use ScrollView for better user experience

--- components/Places/PlaceForm.tsx
+++ components/Places/PlaceForm.tsx
@@ -1,5 +1,41 @@
-import {View} from "react-native";
+import {useState} from "react";
+import {ScrollView, TextInput, View, Text, StyleSheet} from "react-native";
 
 export function PlaceForm() {
-  return <View>PlaceForm</View>;
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitleHandler(enteredTitle: string) {
+    setEnteredTitle(enteredTitle);
+  }
+
+    return (
+      <ScrollView>
+        <View style={styles.form}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={changeTitleHandler}
+            value={enteredTitle}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
